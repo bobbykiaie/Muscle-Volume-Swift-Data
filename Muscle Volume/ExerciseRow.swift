fa76b69397dbba769ee94d539bd77e35c83ee3ee
Bobby Kiaie <bobbykiaie@gmail.com>
2024-02-21 20:18:29 -0800

    - Fix swipe direction for deleting set - Fix keyboard dismissal by allowing user to tap anywhere on screen

---
+++ Muscle Volume/ExerciseRow.swift
@@ -0,0 +1,18 @@
+//
+//  ExerciseRow.swift
+//  Muscle Volume
+//
+//  Created by Babak Kiaie on 2/20/24.
+//
+
+import SwiftUI
+
+struct ExerciseRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExerciseRow()
+}
